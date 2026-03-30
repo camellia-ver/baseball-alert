5adb28fb747fdc48c6f405c431cd426dc24c0361
camellia-ver <jakahi435@gmail.com>
2026-03-30 15:06:15 +0900

    feat: add data filtering

---
+++ data_filtering.py
@@ -0,0 +1,31 @@
+import yaml
+
+TV_MAPPING = {
+    "S-T": "SBS",
+    "M-T": "MBC",
+    "K-2T": "KBS2",
+    "SPO-T": "SPOTV",
+    "SPO-2T": "SPOTV2",
+    "MS-T": "MBC Sports+",
+    "SS-T": "SBS Sports",
+    "KN-T": "KBS N Sports",
+}
+
+def load_config():
+    with open('config.yaml', 'r', encoding='utf-8') as f:
+        return yaml.safe_load(f)
+    
+def filtering_games(games):
+    config = load_config()
+    team = config['team']
+    broadcast = config['broadcast']
+
+    filtered = []
+    for game in games:
+        is_target_team = is_target_team = game['away'] == team or game['home'] == team
+        is_tv_broadcast = TV_MAPPING.get(game['tv'], game['tv']) in broadcast
+
+        if is_target_team and is_tv_broadcast:
+            filtered.append(game)
+
+    return filtered
