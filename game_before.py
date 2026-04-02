58f78d2eaa3701b192dbc54762653beb51571de1
camellia-ver <jakahi435@gmail.com>
2026-04-02 14:23:53 +0900

    chore: update job schedule files

--- game_before.py
+++ game_before.py
@@ -2,12 +2,14 @@ from game_schedule_crawling import get_game_schedule
 from data_filtering import filtering_games
 from sending_kakaotalk import sending_kakaotalk
 from token_manager import ensure_valid_token
+from save_data import save_games
 
 def main():
     ensure_valid_token() # 토큰 유효성 확인 및 갱신
     games = get_game_schedule()
     filtered = filtering_games(games)
     sending_kakaotalk(filtered)
+    save_games(filtered)
 
 if __name__ == '__main__':
     main()
