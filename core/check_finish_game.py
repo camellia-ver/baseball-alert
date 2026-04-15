cdef2fefa09979265abac93e55ef2da6c31a90a3
camellia-ver <jakahi435@gmail.com>
2026-04-15 17:55:18 +0900

    refactor: update if conditions to match new dictionary structure

--- core/check_finish_game.py
+++ core/check_finish_game.py
@@ -2,7 +2,7 @@ def is_game_finished(game):
     if game['remarks'] != '-':  # 취소
         return True
     
-    if game['has_highlight']:   # 정상 종료
+    if game['has_review']:   # 정상 종료
         return True
     
     return False
